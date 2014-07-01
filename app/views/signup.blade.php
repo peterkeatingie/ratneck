8f45a6180b5a59c27f5075c6d7873fd59df7be34
Peter Keating <peter.keating@imaginegroup.ie>
2014-07-01 17:14:00 +0100

    Add some classes to signup form

--- app/views/signup.blade.php
+++ app/views/signup.blade.php
@@ -7,36 +7,68 @@
 
 			<div class="panel-heading">
 				Signup
-				
-				{{ Form::open(array('url' => 'signup/register')) }}
-				
-				{{ Form::label('email', 'Email Address') }}
-				{{ Form::email('email') }}
-				
-				<br />
-				
-				{{ Form::label('alias', 'Alias') }}
-				{{ Form::text('alias') }}
-				
-				<br />
-				
-				{{ Form::label('password', 'Password') }}
-				{{ Form::password('password') }}
-				
-				<br />
-				
-				{{ Form::label('password_confirmation', 'Confirm Password') }}
-				{{ Form::password('password_confirmation') }}
-				
-				<br />
-				
-				{{ Form::submit('Signup') }}
-				
-				{{Form::close()}}
 			</div>
 
 			<div class="panel-body">
+				{{ Form::open(array('url' => 'signup/register', 'role' => 'form')) }}
+				
+				<div class="form-group">
+				
+					{{ Form::label('email', 'Email Address') }}
+					{{ Form::email('email', null, array(
+						'class' => 'form-control',
+						'placeholder' => 'Enter Email Address',
+						)) }}
+					
+					@foreach($errors->get('email') as $message)
+						{{ $message }}
+					@endforeach
+				
+				</div>
+				
+				<div class="form-group">
+					{{ Form::label('alias', 'Alias') }}
+					{{ Form::text('alias', null, array(
+						'class' => 'form-control',
+						'placeholder' => 'Enter Alias',
+						)) }}
+						
+					@foreach($errors->get('alias') as $message)
+						{{ $message }}
+					@endforeach
+					
+				</div>
+				
+				<div class="form-group">
+					{{ Form::label('password', 'Password') }}
+					{{ Form::password('password', array(
+						'class' => 'form-control',
+						'placeholder' => 'Enter Password',
+						)) }}
+						
+					@foreach($errors->get('password') as $message)
+						{{ $message }}
+					@endforeach	
+				</div>
 
+				<div class="form-group">
+					{{ Form::label('password_confirmation', 'Confirm Password') }}
+					{{ Form::password('password_confirmation', array(
+						'class' => 'form-control',
+						'placeholder' => 'Confirm Password',
+						)) }}
+						
+					@foreach($errors->get('password_confirmation') as $message)
+						{{ $message }}
+					@endforeach	
+				</div>	
+				
+				<div class="form-group">
+				
+					{{ Form::submit('Signup') }}
+				</div>
+				
+				{{Form::close()}}
 			</div>
 
 		</div>
