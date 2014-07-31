92713bb158bf41f1be52763443fac8669dd45a20
Peter Keating <peter.keating@imaginegroup.ie>
2014-07-31 12:57:18 +0100

    Change column layout for galleries

--- app/views/images.blade.php
+++ app/views/images.blade.php
@@ -53,9 +53,10 @@
 	
 	<div class="row" id="row-2">
 
-		@foreach($imageGalleries as $imageGallery)
-	
 		<div class="col-md-6">
+	
+		@foreach($galleriesLeft as $imageGallery)
+		
 			<div class="panel panel-default">
 
 			<div class="panel-heading">
@@ -88,9 +89,52 @@
 					</div>
 
 				</div>
+			
+		@endforeach
+		
 		</div>
 		
+		<div class="col-md-6">
+	
+		@foreach($galleriesRight as $imageGallery)
+		
+			<div class="panel panel-default">
+
+			<div class="panel-heading">
+                    
+			<a href="/photos?post={{$imageGallery->slug}}"><strong>{{$imageGallery->name}}</strong></a><br/>
+                    
+		</div>
+			
+					<div class="panel-body">
+						<div class="row">
+						
+						
+						
+							<?php $images = $imageGallery->getImages(); ?>
+							
+							@foreach($images as $image)
+							<div class="col-xs-3 col-md-2 remove-padding-left-right">
+								
+								<a class="fancybox thumbnail" href="{{$imagesFolder}}{{$image->filename}}" class="thumbnail">
+								
+									<img class="mythumb" src="{{$thumbsFolder}}{{$image->getThumbnail()->filename}}" alt="Band">
+								
+									</a>
+								
+							</div>
+							@endforeach
+						
+						
+						</div>
+					</div>
+
+				</div>
+			
 		@endforeach
+		
+		</div>
+
 	</div>
      
     
